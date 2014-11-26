097a8d75fe86b210830c91a3960cdbbf3c4d2f91
Matt Holmes <matthew.allen.holmes@gmail.com>
2014-11-25 16:48:52 -0800

    Updated to build with SNK

--- src/IrcMessageSharp/Properties/AssemblyInfo.cs
+++ src/IrcMessageSharp/Properties/AssemblyInfo.cs
@@ -43,4 +43,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion ("0.5.0.0")]
 [assembly: AssemblyFileVersion ("0.5.0.0")]
 [assembly: AssemblyInformationalVersion ("0.5.0")]
-[assembly: InternalsVisibleTo ("IrcMessageSharp.Spec")]
